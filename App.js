f7377850009375097ff7e13c6090033fa9ade396
nagarajan <nagarajan.r@volantetech.com>
2023-11-16 21:10:31 +0530

    TestM

--- App.js
+++ App.js
@@ -1,40 +1,81 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-//below using JSXElement
-/*
-const jsxHeading = <h1 id="Heading">"Using JSX"</h1>
+/** To create food website
+ * Header
+ *  Logo
+ *  Nav Items
+ * Body
+ *  -Search
+ *  -Restaurant Container
+ *      Restaurnant cards
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contacts
+ * 
+ */
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg"/>
+            </div>
+            <div className = "nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurnantCard = ()=>{
+    return (
+        <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+            <img 
+                className="res-logo" 
+                alt="Foods"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hwbeetykbqbudvijg6yd"
+            />
+            <h3>Buhari</h3>
+            <h4>Biriyani, North Indian</h4>
+            <h5>4.3 stars</h5>
+            <h5>38 mins</h5>
+        </div>
+    );
+};
+
+const Body = ()=>{
+    return (
+        <div className="Body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurnantCard />
+                <RestaurnantCard />
+                <RestaurnantCard />
+                <RestaurnantCard />
+                <RestaurnantCard />
+                <RestaurnantCard />
+                <RestaurnantCard />
+                <RestaurnantCard />               
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = ()=>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-*/
-const Title =() =>(
-    <h1 className="head">Component composition</h1>
-)
-//Javascript title
-const title = (
-    <h1 className="head">Component composition</h1>
-)
 
-
-
-//React functional component - below is the one way to introduce react element inside component
-/*const HeadingComponent = ()=>(
-    <div id="container">
-    <Title/>
-    <h1 id="Heading">RFC</h1>
-    </div>
-)*/
-
-//Second method to define the const variable Title inside react component as javascript code
-const HeadingComponent = ()=>(
-    <div id="container">
-    {title}
-    {Title()}
-    {<Title></Title>}
-    {<Title/>}
-    <h1 id="Heading">RFC</h1>
-    </div>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(<HeadingComponent />)
+root.render(<AppLayout />)
